64179508860b5d6e02370f7f68ba053d9173b5e7
Sakeerin <theblue0800@gmail.com>
2025-05-04 20:37:56 +0700

    setup project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import {  } from 'vue-router'
+</script>
+
+<template>
+  <header>
+      <nav>
+        <RouterLink to="/" class="nav-link">Home</RouterLink>
+      </nav>
+  </header>
+    <RouterView/>
+</template>
+
+
